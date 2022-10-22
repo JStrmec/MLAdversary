34f5d9e963579d67f4447e39901a0f9a03843117
Jocelyn Strmec <strj_vienna@yahoo.com>
2022-10-21 18:01:55 -0700

    comment chge

--- model.py
+++ model.py
@@ -148,16 +148,11 @@ class Model:
         # salt and pepper noise attack
         # sapn = fb.attacks.SaltAndPepperNoiseAttack()
 
-        # # set attack parameters
-        # fgsm_epsilons = np.linspace(0.02, 0.02, num=1)
-        # inv_epsilons = np.linspace(10, 10, num=1)
-        # sapn_epsilons = np.linspace(80, 80, num=1)
-
         # # run attack
-        # raw, fgsm_advs_list, success = fgsm(self.model, data, [], epsilons=fgsm_epsilons)
+        # raw, fgsm_advs_list, success = fgsm(self.model, data, labels, epsilons=epsilons)
         # print("FGSM: ",raw, fgsm_advs_list, success)
-        # raw, inv_advs_list, success = inversion(self.model, data, [], epsilons=inv_epsilons)
+        # raw, inv_advs_list, success = inversion(self.model, data, labels, epsilons=epsilons)
         # print("INV: ",raw, inv_advs_list, success)
-        # raw, sapn_advs_list, success = sapn(self.model, data, [], epsilons=sapn_epsilons)
+        # raw, sapn_advs_list, success = sapn(self.model, data, labels, epsilons=epsilons)
         # print("SAPN: ",raw, sapn_advs_list, success)
         return adversarial
